ca1ac680b4fa03e32037710bf17027d190c27199
Jason Cheung <jcheung7@stuy.edu>
2018-04-15 17:51:49 -0400

    minor design improvements

--- MazeSolver.java
+++ MazeSolver.java
@@ -5,50 +5,48 @@
  */
 
 public class MazeSolver {
-	private Maze maze;
-	private boolean solution;
-	private Displayer displayer;
+    private Maze maze;
+    private boolean solution;
+    private Displayer displayer;
+    
+    private static final int[] DIRECTIONS = {Maze.EAST, Maze.SOUTH, Maze.WEST, Maze.NORTH};
 
-	public MazeSolver(Maze mazeToBeSolved, int windowHeight) {
+    public MazeSolver(Maze mazeToBeSolved, int windowHeight) {
+	maze = mazeToBeSolved;
+	displayer = new Displayer(windowHeight);
+	displayer.atTopOfWindow("solving maze:" + System.lineSeparator() + maze);
+	solution = solveMaze();
+    }
 
-		displayer = new Displayer(windowHeight);
-
-		maze = mazeToBeSolved;
-		displayer.atTopOfWindow("solving maze:" + System.lineSeparator() + maze);
-		solution = solveMaze();
-	}
-
-	private boolean solveMaze() {
-
-	    int[] directions = {Maze.EAST, Maze.SOUTH, Maze.WEST, Maze.NORTH};
-	 
-	    if (maze.explorerIsOnA() == Maze.TREASURE)
-		return true;
-	    else if (maze.explorerIsOnA() == Maze.WALL)
-		return false;
-	    else {
-		
-			  Maze snapshot = new Maze(maze);
-		for (int direction : directions) {
-
+    private boolean solveMaze() {
 		    
-		    maze.dropA(Maze.WALL);
-		    maze.go(direction); 
-		    displayer.atTopOfWindow(maze.toString());
-		    if (solveMaze())
-			return true;
-		    maze = new Maze(snapshot);
-		    displayer.atTopOfWindow(maze.toString());
-		}
-
-		return false;
+	if (maze.explorerIsOnA() == Maze.TREASURE)
+	    return true;
+	else if (maze.explorerIsOnA() == Maze.WALL)
+	    return false;
+	else {
+		
+	    Maze snapshot = new Maze(maze);
+	    for (int direction : DIRECTIONS) {
+		    
+		maze.dropA(Maze.WALL);
+		maze.go(direction); 
+		displayer.atTopOfWindow(maze.toString());
+		if (solveMaze())
+		    return true;
+		maze = new Maze(snapshot);
+		displayer.atTopOfWindow(maze.toString());
+		    
 	    }
-	}
 
-	public String toString() {
-		if (solution)
-			return "A solution exists";
-		else 
-			return "no solution";
+	    return false;
 	}
+    }
+
+    public String toString() {
+	if (solution)
+	    return "A solution exists";
+	else 
+	    return "no solution";
+    }
 }
